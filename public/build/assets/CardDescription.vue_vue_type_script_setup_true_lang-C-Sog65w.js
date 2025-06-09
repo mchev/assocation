f4850ef53ee377699ffc62583fa52b39f2ab5260
Martin Chevignard <martin.chevignard@gmail.com>
2025-06-09 21:26:15 +0200

    removing build from gitignore

---
+++ public/build/assets/CardDescription.vue_vue_type_script_setup_true_lang-C-Sog65w.js
@@ -0,0 +1,1 @@
+import{c as o}from"./index-DnfeQZQ1.js";import{d as t,k as r,o as n,a as c,y as p,u as l}from"./app-D_qiWvfj.js";const u=t({__name:"CardDescription",props:{class:{}},setup(s){const e=s;return(a,m)=>(n(),r("p",{"data-slot":"card-description",class:p(l(o)("text-muted-foreground text-sm",e.class))},[c(a.$slots,"default")],2))}});export{u as _};
---
+++ public/build/assets/check-BR4bOqko.js
@@ -0,0 +1,6 @@
+import{q as r,h as t}from"./Input.vue_vue_type_script_setup_true_lang-CwD4nx7U.js";import{b as c}from"./app-D_qiWvfj.js";import{c as m}from"./createLucideIcon-Xz3TQGcV.js";function u(o){return c(()=>{var e;return r(o)?!!((e=t(o))!=null&&e.closest("form")):!0})}/**
+ * @license lucide-vue-next v0.468.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const f=m("CheckIcon",[["path",{d:"M20 6 9 17l-5-5",key:"1gmf2c"}]]);export{f as C,u};
