66b79f339b1ce774640c197722ef6efbad5e47e5
Martin Chevignard <martin.chevignard@gmail.com>
2025-06-21 16:05:35 +0200

    Disabling Assets middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -28,7 +28,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->web(append: [
             HandleAppearance::class,
             HandleInertiaRequests::class,
-            AddLinkHeadersForPreloadedAssets::class,
+            // AddLinkHeadersForPreloadedAssets::class,
         ]);
 
         $middleware->alias([
